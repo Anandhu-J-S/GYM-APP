574a93ee97e135f8a6b2780d35e2fb75ac461bac
Anandhu <anandhujs.srishti@gmail.com>
2025-02-03 09:53:31 +0530

    deleted Test Files

--- gym_app_server/src/main/java/com/gym_app/gym_app_server/Repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gym_app.gym_app_server.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gym_app.gym_app_server.Modal.TestModal;
-
-public interface TestRepository extends JpaRepository<TestModal,Integer> {
-    
-}
